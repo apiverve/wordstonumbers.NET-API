bba2148ad23379886e7947006839b380ba87d340
APIVerve Team <hello@apiverve.com>
2024-08-20 13:36:39 -0500

    Bug Fixes

---
+++ WordstoNumbersQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class WordstoNumbersQueryOptions {
+public string words { get; set; }
+}
+
+}
